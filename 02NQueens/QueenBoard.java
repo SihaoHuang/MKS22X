ecb131f733b389e337d69e7d6dd5bf6bf920cfdf
Sihao Huang <astronomywong@gmail.com>
2017-02-13 22:53:36 -0500

    Final commit

--- 02NQueens/QueenBoard.java
+++ 02NQueens/QueenBoard.java
@@ -16,11 +16,43 @@ public class QueenBoard{
      *final configuration of the board after adding 
      *all n queens. Uses solveH
      */
-    public boolean solve(){
-	    return solveH(0);
+    public void countSolutions(){
+	    countSolutionsH(0);
+    }
+
+    public void solve(){
+      solveH(0);
+      int ifSolved = 0;
+      for(int each:board[0]){
+        if(each != 0) ifSolved ++;
+      }
+      if(ifSolved > 0){
+        for(int i = 0; i < board.length; i ++){
+          if(board[i][board.length - 1] == 0) board[i][board.length - 1] = -1;
+        }
+      }
     }
 
     private boolean solveH(int col){
+      if(col == board.length - 1 && hasPlaceable(col)){
+        return true; //this is okay as the last col will have one unique solution for each set of conditions
+      }
+      if(col == board.length - 1 && !hasPlaceable(col)){
+        return false;
+      }
+      //recursive call
+      else{
+        for(int row = 0; row < board.length; row ++){
+          if (placeQueen(row, col)){
+            if (!solveH(col+1)) removeQueen(row, col);
+            else return true; //quit loop
+          }
+        }
+        return solutionCount != 0;
+      }
+    }
+
+    private boolean countSolutionsH(int col){
       //System.out.println(toString());
       //base cases
       //System.out.println("on column "+col);
@@ -37,7 +69,7 @@ public class QueenBoard{
       else{
         for(int row = 0; row < board.length; row ++){
           if (placeQueen(row, col)){
-            solveH(col+1);
+            countSolutionsH(col+1);
             removeQueen(row, col);
           }
         }
@@ -130,7 +162,7 @@ public class QueenBoard{
       for(int[] row:board){
         for(int each:row){
           if(each == -1) output += "Q ";
-          else output += Integer.toString(each) + " ";
+          else output += "_ ";
         }
         output += "\n";
       }
@@ -142,7 +174,7 @@ public class QueenBoard{
     }
 
     public static void main(String[] args){
-      QueenBoard test = new QueenBoard(1);
+      QueenBoard test = new QueenBoard(30);
       // test.placeQueen(5,4);
       // test.placeQueen(3,3);
       // test.toString();
@@ -151,7 +183,7 @@ public class QueenBoard{
       // System.out.println("here");
       // System.out.println(test.toString());
       test.solve();
-      System.out.println(test.getSolutionCount());
+      System.out.println(test.toString());
       //System.out.println(test.toString());
     }
 }
