bb4491b04ec8e398ae76c4726e1048dab355b48f
Sihao Huang <astronomywong@gmail.com>
2017-02-27 18:21:37 -0500

    Profiled functions for runtime

--- EC01/KnightBoard.java
+++ EC01/KnightBoard.java
@@ -1,10 +1,14 @@
 import java.util.Arrays;
 import java.lang.Math;
+
+//NOTE: this implementation of the Warnsdorff's rule works as intended when printed out/animated, but it does not work for boards 11*11 and above. I tried profiling each of the functions but there doesnt seem to be a significant issue.
 public class KnightBoard{
   private int[][] board;
   private int[][] matrix;
   private int[] rowOrder = {2,1,-1,-2,-2,-1,1,2};
   private int[] colOrder = {1,2,2,1,-1,-2,-2,-1};
+  private static long genQueElapsedTime;
+  private static long findSmallElapsedTime;
 
   public KnightBoard(int startingRows,int startingCols){
     board = new int[startingRows][startingCols];
@@ -37,8 +41,10 @@ public class KnightBoard{
       int[] queue = generateQueue(row, col);
       //System.out.println(Arrays.toString(queue));
       while(!isEmptyArray(queue)){
-        //System.out.println(toString());
+        System.out.println(toString());
         int i = findSmallest(queue);
+        //System.out.println("genQueue is " + genQueElapsedTime);
+        //System.out.println("findSmallest is " + findSmallElapsedTime);
         //System.out.println(i);
         if(solveFastH(row + rowOrder[i], col + colOrder[i], id + 1)) return true;
       }
@@ -49,6 +55,7 @@ public class KnightBoard{
 
 /******************************************************AUX FUNCTIONS****************************************************************/
   private int[] generateQueue(int row, int col){ //Generates a queue of moves surrounding a square clockwise, starting from 1 o clock direction. Constant time!
+    //long genQueStart = System.nanoTime();  
     int[] out = new int[8];
     for(int i = 0; i < 8; i ++){
       if(((row + rowOrder[i]) >= 0) && ((row + rowOrder[i]) < board.length) && ((col + colOrder[i]) >= 0) && ((col + colOrder[i]) < board[0].length)){
@@ -56,10 +63,12 @@ public class KnightBoard{
       }
       else out[i] = 0;
     }
+    //genQueElapsedTime = System.nanoTime() - genQueStart;
     return out;
   }
 
   public static int findSmallest(int[] in){ //Finds the smallest number in the queue, returns it so solveFastH can attempt it first, and then sets that to zero. Constant time!
+    //long findSmallStart = System.nanoTime();
     int smallestInd = 0;
     for(int i = 0; i < 8; i++){
       if((in[i] != 0) && ((in[i] < in[smallestInd]) || (in[smallestInd] == 0))){
@@ -67,6 +76,7 @@ public class KnightBoard{
       }
     }
     in[smallestInd] = 0;
+    //findSmallElapsedTime = System.nanoTime() - findSmallStart;
     return smallestInd;
   }
 
@@ -118,19 +128,6 @@ public class KnightBoard{
   }
 
 }
-
-/*
-Optimization:
-Code the array with the number of options for movement. Update that as you move. Prioritize moves based on smallest. Initialize an extra array.
-2 3 4 4 4 3 2
-3 4 6 6 6 4 3
-4 6 5 5 5 6 4
-4 6 5 5 5 6 4 
-4 6 5 5 5 6 4 
-3 4 6 6 6 4 3
-2 3 4 4 4 3 2
-*/
-
 
 
 
