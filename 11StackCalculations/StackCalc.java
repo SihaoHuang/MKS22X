6f5771943dd398a8e9dff2bb84538a4ce6dbf314
Huang Sihao <Huang Sihao>
2017-06-02 00:20:42 -0400

    fixed; operations were flipped

--- 11StackCalculations/StackCalc.java
+++ 11StackCalculations/StackCalc.java
@@ -2,10 +2,10 @@ import java.util.*;
 public class StackCalc{
     public static Double apply(String op, Double a, Double b){
         if(op.equals("+")) return a + b;
-        else if(op.equals("-")) return a - b;
+        else if(op.equals("-")) return b - a;
         else if(op.equals("*")) return a * b;
-        else if(op.equals("/")) return a / b;
-        else if(op.equals("%")) return a % b;
+        else if(op.equals("/")) return b / a;
+        else if(op.equals("%")) return b % a;
         else return 0.00001234; // for debugging, just in case
     }
 
@@ -23,8 +23,8 @@ public class StackCalc{
         return (Double)buffer.pop();
     }
 
-    public static void main(String[] args){
-        System.out.println(eval("1 5 4 + -12.2 * /"));
-        System.out.println(eval("1 2 +"));
-    }
+    // public static void main(String[] args){
+    //     System.out.println(eval("1 5 4 + -12.2 * /"));
+    //     System.out.println(eval("1 2 +"));
+    // }
 }
